0713f3a55b44a5338182366e91ad9762ee998ab3
zura <zuramanagadze10@gmail.com>
2024-10-09 05:53:29 +0400

    ready to deploy

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,27 @@
+import path from "path";
 import mongoose from "mongoose";
 import app from "./app.js";
 import dotenv from "dotenv";
+import express from "express";
 dotenv.config();
+const __dirname = path.resolve();
+
+console.log(__dirname);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(
+      path.resolve(__dirname, "frontend", "dist", "index.html"),
+      (err) => {
+        if (err) {
+          res.status(err.status).send(err.message);
+        }
+      }
+    );
+  });
+}
 
 mongoose
   .connect(process.env.DATABASE_URI)
